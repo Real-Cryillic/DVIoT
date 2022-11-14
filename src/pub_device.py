91ecafa6a8fc8abf08fcf7caa4b527f2cd5a0944
Real-Cryillic <cunningham.dom@outlook.com>
2022-11-14 16:38:17 -0500

    update random seeding

--- src/pub_device.py
+++ src/pub_device.py
@@ -12,6 +12,14 @@ port = 1883
 topic = "device/init"
 client_id = f"{random.randint(0,1000)}"
 
+def generate_id():
+    random.seed(0)
+    return "".join(
+        random.SystemRandom().choice(string.ascii_uppercase + string.digits)
+        for _ in range(20)
+    )
+
+id = generate_id()
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -29,21 +37,13 @@ def connect_mqtt() -> mqtt_client:
 def publish(client: mqtt_client):
     while True:
         time.sleep(10)
-        msg = f"{generate_id()}"
+        msg = f"{id}"
         result = client.publish(topic, msg)
         status = result[0]
         if status == 0:
             print(f"Send `{msg}` to topic `{topic}`")
         else:
             print(f"Failed to send message to topic {topic}")
-
-
-def generate_id():
-    random.seed(0)
-    return "".join(
-        random.SystemRandom().choice(string.ascii_uppercase + string.digits)
-        for _ in range(20)
-    )
 
 
 def main():
