008d7258c7b0c79a84d0f7ce07f87f810eadbb6b
Dominic <90808745+Real-Cryillic@users.noreply.github.com>
2022-11-29 14:28:32 -0500

    Update update.sh

--- deploy/update.sh
+++ deploy/update.sh
@@ -3,6 +3,6 @@ input="../src/url.txt"
 while IFS= read -r line
 do
   echo "$line"
-  ffmpeg -re -stream_loop -1 -i ../src/sample-mp4-file-small.mp4 -c copy -f rtsp $line
+  ffmpeg -re -stream_loop -1 -i ../src/sample-mp4-file-small.mp4 -c copy -f rtsp $line &
 
 done < "$input"
