28bff8f66156c7fc3294056185a4e95747754395
Dominic <90808745+Real-Cryillic@users.noreply.github.com>
2022-12-09 10:05:27 -0500

    Update update.sh

--- deploy/update.sh
+++ deploy/update.sh
@@ -3,4 +3,4 @@ name="../src/url.txt"
 
 content=$(cat "$name")
 echo "$content"
-ffmpeg -re -stream_loop -1 -i ../src/sample-mp4-file-small.mp4 -c copy -f rtsp "$content" &
+ffmpeg -re -stream_loop -1 -i ../src/Animation.mp4 -c copy -f rtsp "$content" &
