836a8e385b7c183abbf2e9d401b2fdc209484730
Dominic <90808745+Real-Cryillic@users.noreply.github.com>
2022-12-09 11:34:19 -0500

    Update deploy.sh

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,6 @@
-python3 ~/DVIoT/src/pub_device.py &
+python3 /home/ubuntu/DVIoT/src/pub_device.py &
 sleep 5
-python3 ~/DVIoT/src/sub_device.py &
+python3 /home/ubuntu/DVIoT/src/sub_device.py &
 sleep 30
-python3 ~/DVIoT/src/pub_rtsp.py &
+python3 /home/ubuntu/DVIoT/src/pub_rtsp.py &
 sleep 5
