97da5be1c0102a439cf7b51a049bb53e0631afec
Dominic <90808745+Real-Cryillic@users.noreply.github.com>
2022-12-09 11:05:15 -0500

    Update update.sh

--- deploy/update.sh
+++ deploy/update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-name="../src/url.txt"
+name="~/DVIoT/src/url.txt"
 
 content=$(cat "$name")
 echo "$content"
-ffmpeg -re -stream_loop -1 -i ../src/Animation.mp4 -c copy -f rtsp "$content" &
+ffmpeg -re -stream_loop -1 -i ~/DVIoT/src/Animation.mp4 -c copy -f rtsp "$content" &
